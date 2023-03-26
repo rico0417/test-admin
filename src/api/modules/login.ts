5c700c8db2ff99931ca36b4fd8dd9f507620b28d
Rico <>
2023-03-26 22:07:47 +0800

    更新

--- src/api/modules/login.ts
+++ src/api/modules/login.ts
@@ -1,7 +1,5 @@
 import { Login } from "@/api/interface/index";
 import { PORT1 } from "@/api/config/servicePort";
-import DynamicRouter from "@/assets/json/dynamicRouter.json";
-import AuthButtons from "@/assets/json/authButtons.json";
 import qs from "qs";
 import http from "@/api";
 
@@ -20,15 +18,11 @@ export const loginApi = (params: Login.ReqLoginForm) => {
 // * 获取按钮权限
 export const getAuthButtonListApi = () => {
 	return http.get<Login.ResAuthButtons>(PORT1 + `/auth/buttons`, {}, { headers: { noLoading: true } });
-	// 如果想让按钮权限变为本地数据，注释上一行代码，并引入本地 authButtons.json 数据
-	return AuthButtons;
 };
 
 // * 获取菜单列表
 export const getAuthMenuListApi = () => {
 	return http.get<Menu.MenuOptions[]>(PORT1 + `/menu/list`, {}, { headers: { noLoading: true } });
-	// 如果想让菜单变为本地数据，注释上一行代码，并引入本地 dynamicRouter.json 数据
-	return DynamicRouter;
 };
 
 // * 用户退出登录
